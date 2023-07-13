094e162025075ba46740933cf2e0c24b6ae65794
calmbeing <99724749+calmbeing@users.noreply.github.com>
2023-07-13 10:21:36 +0800

    bump version of bsc-erigon (#161)

--- params/version.go
+++ params/version.go
@@ -33,7 +33,7 @@ var (
 const (
 	VersionMajor       = 1     // Major version component of the current release
 	VersionMinor       = 1     // Minor version component of the current release
-	VersionMicro       = 3     // Patch version component of the current release
+	VersionMicro       = 4     // Patch version component of the current release
 	VersionModifier    = "dev" // Modifier component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
