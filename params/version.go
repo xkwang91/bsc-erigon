74b70a2a081e4be63b9e22d5b05c03e79201ebd4
Peter Chung <126496711+pc-quiknode@users.noreply.github.com>
2023-08-10 10:54:47 +0800

    bump version

--- params/version.go
+++ params/version.go
@@ -33,7 +33,7 @@ var (
 const (
 	VersionMajor       = 1     // Major version component of the current release
 	VersionMinor       = 1     // Minor version component of the current release
-	VersionMicro       = 6     // Patch version component of the current release
+	VersionMicro       = 7     // Patch version component of the current release
 	VersionModifier    = "dev" // Modifier component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
